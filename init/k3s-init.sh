7035dcafbb6db9c83f800829c10fddf4bc9e3199
Jakub Zilinek <zilinek.jakub@gmail.com>
2025-01-26 13:01:04 +0100

    removed values.yaml

--- init/k3s-init.sh
+++ init/k3s-init.sh
@@ -4,4 +4,5 @@ k3s server \
 --kube-controller-manager-arg address=0.0.0.0 \
 --kube-controller-manager-arg bind-address=0.0.0.0 \
 --kube-scheduler-arg bind-address=0.0.0.0 \
---kube-scheduler-arg address=0.0.0.0
+--kube-scheduler-arg address=0.0.0.0 \
+--disable=servicelb,traefik
