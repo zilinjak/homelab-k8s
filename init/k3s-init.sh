b7a406036b9382d399dc809b0341733efdc3629a
zilinjak <zilinek.jakub@gmail.com>
2025-02-01 19:54:41 +0100

    added metal lb

--- init/k3s-init.sh
+++ init/k3s-init.sh
@@ -1,8 +1,14 @@
-k3s server \
---etcd-expose-metrics true \
---kube-proxy-arg metrics-bind-address=0.0.0.0 \
---kube-controller-manager-arg address=0.0.0.0 \
---kube-controller-manager-arg bind-address=0.0.0.0 \
---kube-scheduler-arg bind-address=0.0.0.0 \
---kube-scheduler-arg address=0.0.0.0 \
---disable=servicelb,traefik
+curl -sfL https://get.k3s.io > k3s.sh
+chmod +x k3s.sh
+
+./k3s.sh server \
+    --disable=servicelb \
+    --disable=traefik \
+    --etcd-expose-metrics true \
+    --kube-proxy-arg metrics-bind-address=0.0.0.0 \
+    --kube-controller-manager-arg address=0.0.0.0 \
+    --kube-controller-manager-arg bind-address=0.0.0.0 \
+    --kube-scheduler-arg bind-address=0.0.0.0 \
+    --kube-scheduler-arg address=0.0.0.0
+
+sudo cat /etc/rancher/k3s/k3s.yaml
