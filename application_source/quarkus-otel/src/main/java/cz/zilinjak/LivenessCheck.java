a363382d75f67c8a610d1e1a748ba3f000405882
zilinjak <zilinek.jakub@gmail.com>
2025-12-29 20:21:51 +0100

    added quarkus-otel app

---
+++ application_source/quarkus-otel/src/main/java/cz/zilinjak/LivenessCheck.java
@@ -0,0 +1,17 @@
+package cz.zilinjak;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@Liveness
+@ApplicationScoped
+public class LivenessCheck implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.up("Application is live");
+    }
+}
---
+++ application_source/quarkus-otel/src/main/java/cz/zilinjak/ReadinessCheck.java
@@ -0,0 +1,17 @@
+package cz.zilinjak;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Readiness;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@Readiness
+@ApplicationScoped
+public class ReadinessCheck implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.up("Application is ready");
+    }
+}
